d787e10fb58265923e0b89da5dec0c66b041f761
Zsuzsanna1991 <zsuzsannaszabo04@gmail.com>
2026-04-02 12:36:41 +0200

    NuGet packages are installed.

--- ElectricShop/obj/Debug/net8.0/ElectricShop.AssemblyInfo.cs
+++ ElectricShop/obj/Debug/net8.0/ElectricShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElectricShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e501dec44427a1691646d9dbabbcbb42c6d26b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElectricShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElectricShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
