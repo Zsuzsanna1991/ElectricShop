de302e919813d91581f2a5215be8cf9ad3038472
Zsuzsanna1991 <zsuzsannaszabo04@gmail.com>
2026-04-02 12:49:31 +0200

    Computer and OSystem models are created.

---
+++ ElectricShop/Models/Computer.cs
@@ -0,0 +1,17 @@
+﻿namespace ElectricShop.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public int Memory { get; set; }
+        public string Display {  get; set; }
+
+
+        public int OperationSystemId { get; set; }
+
+        //Kapcsolat kialakítása
+        public OperationSystem OperationSystem { get; set; }
+    }
+}
---
+++ ElectricShop/Models/OperationSystem.cs
@@ -0,0 +1,14 @@
+﻿namespace ElectricShop.Models
+{
+    public class OperationSystem
+    {
+        public int Id { get; set; }
+        public string Name {  get; set; }
+        public string Version { get; set; }
+
+
+        //Kapcsolat kialaktása
+        public ICollection<Computer> computers { get; set; } = new List<Computer>();
+        
+    }
+}
